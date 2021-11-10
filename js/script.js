06d98c3040317408d8dd7de22ab5b54a59360bee
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-10 14:05:14 +0100

    fix dadi

--- js/script.js
+++ js/script.js
@@ -23,7 +23,9 @@ if (sentinella) {
 // ************ Dadi ***********
 function esito() {
     const dado1 = Math.round(Math.random() * 5) + 1;
+    document.getElementById("dado1").innerHTML = dado1;
     const dado2 = Math.round(Math.random() * 5) + 1;
+    document.getElementById("dado2").innerHTML = dado2;
     if (dado1 > dado2){
         document.getElementById("esito").innerHTML = 'Il Giocatore 1 ha vinto!';
     } else if (dado1 == dado2) {
